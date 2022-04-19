ca936c64e528933b1348517f7a80e82478680a00
Dino <dino@dd9.dev>
2022-04-19 13:40:58 -0400

    Update to 1.0.4

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# Node.js HyperAuth Library
+![](https://i.imgur.com/akqOTpn.png)
 
-[![Version](https://badge.fury.io/js/hyperauth.svg)](https://www.npmjs.org/package/hyperauth)
-[![Downloads](https://img.shields.io/npm/dm/hyperauth.svg)](https://www.npmjs.com/package/hyperauth)
+<h1 style="text-align:center">
+HyperAuth Node.JS Server Library
+</h1>
+
+<div style="display: flex; justify-content: space-between; align-items: center;">
+![](https://img.shields.io/github/stars/hyperteksolutions/hyperauth.svg) ![](https://img.shields.io/github/forks/hyperteksolutions/hyperauth.svg) ![](https://img.shields.io/github/tag/hyperteksolutions/hyperauth.svg) ![](https://img.shields.io/github/release/hyperteksolutions/hyperauth.svg) ![](https://img.shields.io/github/issues/hyperteksolutions/hyperauth.svg)
+</div>
+
+---
+
+<div style="display: flex; justify-content: space-between; align-items: center;">
+[![Npm package version](https://badgen.net/npm/v/hyperauth)](https://npmjs.com/package/hyperauth)
+[![NPM Package Downloads](https://badgen.net/npm/dt/hyperauth)](https://npmjs.com/package/hyperauth)
 [![Try on RunKit](https://badge.runkitcdn.com/hyperauth.svg)](https://runkit.com/npm/hyperauth)
-[![License](https://img.shields.io/github/license/Hypertek-Solutions/hyperauth)](https://github.com/Hypertek-Solutions/hyperauth/blob/master/LICENSE)
+[![License](https://img.shields.io/github/license/hyperteksolutions/hyperauth)](https://github.com/Hypertek-Solutions/hyperauth/blob/master/LICENSE)
+[![Dashboard](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](https://hyperauth.app/)
+</div>
+
+---
 
 The HyperAuth Node library allows for easy integration of HyperAuth APIs for applications written in server-side JavaScript.
 
@@ -13,7 +28,7 @@ See the [API Docs](https://hyperauth.app/docs)
 
 ## Requirements
 
-Node 8, 10 or higher.
+Node 16 or higher.
 
 ## Installation
 
@@ -27,9 +42,12 @@ yarn add hyperauth
 
 ## Usage
 
-The package requires configuration with your API key and Secret Key, which are available in the [TaxHype Dashboard](https://dashboard.hyperauth.app/).
+The package requires configuration with your API key and Secret Key, which are available in the [HyperAuth Dashboard](https://dash.hyperauth.app/).
 
 ```javascript
-const hyperauth = require("hyperauth");
-const client = new hyperauth("API_KEY", "SECRET_KEY");
+const hyperauth = require("hyperauth")("API_KEY", "API_SECRET");
 ```
+
+## Questions / Suggestions
+
+For all questions and suggestions, please create a new issue [here](https://github.com/hyperteksolutions/hyperauth/issues/new) or contact [contact@hypertek.dev](mailto:contact@hypertek.dev).
--- src/index.js
+++ src/index.js
@@ -1,79 +1,127 @@
 const axios = require("axios");
 
-module.exports = class HyperAuth {
-  constructor(api_key, api_secret) {
-    this.api_key = api_key;
-    this.api_secret = api_secret;
+class HyperAuth {
+  constructor(key, secret) {
+    this.key = key;
+    this.secret = secret;
     this.instance = axios.create({
       baseURL: "https://api.hyperauth.app/",
       timeout: 15000,
-      headers: { key: this.api_key, secret: this.api_secret },
+      headers: { key: this.key, secret: this.secret },
     });
   }
+  checkHealth = async () => {
+    try {
+      let x = await this.instance.get("/api/check");
+      return Promise.resolve(x.data);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
   createTenant = async (phoneNumber, email) => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.get("/api/createTenant", {
-          phoneNumber,
-          email,
-        });
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
+    try {
+      let x = await this.instance.get("/api/createTenant", {
+        phoneNumber,
+        email,
+      });
+      return Promise.resolve(x.data);
+    } catch (error) {
+      return Promise.reject(error);
     }
   };
   enrollmentSMS = async (tenantId) => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.post("/api/smsEnroll", { tenantId });
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
+    try {
+      let x = await this.instance.post("/api/smsEnroll", { tenantId });
+      return Promise.resolve(x.data);
+    } catch (error) {
+      return Promise.reject(error);
     }
   };
   authenticate = async (method, tenantId) => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.post("/api/authTenant", {
-          method,
-          tenantId,
-        });
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
+    try {
+      let x = await this.instance.post("/api/authTenant", {
+        method,
+        tenantId,
+      });
+      return Promise.resolve(x.data);
+    } catch (error) {
+      return Promise.reject(error);
     }
   };
   authStatus = async (requestId) => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.post("/api/checkAuth", { requestId });
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
+    try {
+      let x = await this.instance.post("/api/checkAuth", { requestId });
+      return Promise.resolve(x.data);
+    } catch (error) {
+      return Promise.reject(error);
     }
   };
   smsCode = async (requestId, token) => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.post("/api/smsAuth", { requestId, token });
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
+    try {
+      let x = await this.instance.post("/api/smsAuth", { requestId, token });
+      return Promise.resolve(x.data);
+    } catch (error) {
+      return Promise.reject(error);
     }
   };
+  getNewHyperlink = async (tenantId) => {
+    try {
+      let x = await this.instance.post("/api/getHyperlink", { tenantId });
+      return Promise.resolve(x.data);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  verifyOTP = async (tenantId, otp) => {
+    try {
+      let x = await this.instance.post("/api/verifyOTPCode", {
+        tenantId,
+        token: otp,
+      });
+      return Promise.resolve(x.data);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  createPrompt = async (sourceURLs, callbackURL, hyperDomain) => {
+    try {
+      let x = await this.instance.post("/api/createPrompt", {
+        sourceURLs,
+        callbackURL,
+        hyperDomain,
+      });
+      return Promise.resolve(x.data);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  createPromptSession = async (tenantId, token) => {
+    try {
+      let x = await this.instance.post("/api/newPromptSession", {
+        tenantId,
+        token,
+      });
+      return Promise.resolve(x.data);
+    } catch (e) {
+      next(e);
+    }
+  };
+  getPromptSession = async (requestId) => {
+    try {
+      let x = await this.instance.post("/api/getPromptSession", {
+        requestId,
+      });
+      return Promise.resolve(x.data);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+}
+
+var _old = HyperAuth;
+HyperAuth = function (...args) {
+  return new _old(...args);
 };
+
+module.exports = HyperAuth;
+module.exports.default = HyperAuth;
